9e489829fc8f7c569f8cb1c5031816afcaa992fa
jonschlinkert <jon.schlinkert@sellside.com>
2015-08-12 15:21:09 -0400

    rename var

--- index.js
+++ index.js
@@ -1,27 +1,20 @@
-/*!
- * pad-left <https://github.com/jonschlinkert/pad-left>
- *
- * Copyright (c) 2014 Jon Schlinkert, contributors.
- * Licensed under the MIT license.
- */
-
 'use strict';
 
 var repeat = require('repeat-string');
 
 module.exports = function padLeft(val, num, str) {
   var padding = '';
-  var postfix = num - val.length;
+  var diff = num - val.length;
 
   // Breakpoints based on benchmarks to use the fastest approach
   // for the given number of zeros
-  if (postfix <= 5 && !str) {
+  if (diff <= 5 && !str) {
     padding = '00000';
-  } else if (postfix <= 25 && !str) {
+  } else if (diff <= 25 && !str) {
     padding = '000000000000000000000000000';
   } else {
-    return val + repeat(str || '0', postfix);
+    return val + repeat(str || '0', diff);
   }
 
-  return val + padding.slice(-postfix);
+  return val + padding.slice(-diff);
 };
